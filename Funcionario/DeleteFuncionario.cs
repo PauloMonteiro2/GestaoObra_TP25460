f65810fde5f8d493cae774adfe0f5a1c661630c2
PauloMonteiro2 <a25460@alunos.ipca.pt>
2024-12-15 16:19:02 +0000

    1.1

---
+++ Funcionario/DeleteFuncionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestaoObra_TP25460.Funcionario
+{
+    public partial class DeleteFuncionario : Form
+    {
+        public DeleteFuncionario()
+        {
+            InitializeComponent();
+        }
+    }
+}
